060d241af96311de63ee20bc39a8b7f2a82c7709
Rizki Putra Pratamau <rizkiputrapratama47@gmail.comm>
2026-05-12 22:57:52 +0700

    update page buttonw

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 function showPage(pageId) {
   const pages = document.querySelectorAll('.page');
+  const buttons = document.querySelectorAll('nav button');
 
   pages.forEach(page => {
     page.classList.remove('active');
   });
 
+  buttons.forEach(button => {
+    button.classList.remove('active-btn');
+  });
+
   document.getElementById(pageId).classList.add('active');
+
+  event.target.classList.add('active-btn');
 }
